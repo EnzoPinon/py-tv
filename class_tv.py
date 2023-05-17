373ca2d00bdb529b8c803dea8144784a59080985
EnzoPinon <mrlorenzopinon@gmail.com>
2023-05-18 00:49:39 +0800

    added viewing methods to class TV

--- class_tv.py
+++ class_tv.py
@@ -5,3 +5,17 @@ class TV:
         self.channel = channel
         self.is_on = is_on
     
+    def view(self):
+        print("====================\nTV information\n====================")
+        print("TV name: ", self.name, "\nCurrent volume: ", self.volume, "\nCurrent channel: ", self.channel)
+        if self.is_on is True:
+            print("TV is on?: YES\n")
+        else:
+            print("TV is on?: NO\n")
+    
+    def get_volume(self):
+        print("The volume of ", self.name, " is ", self.volume)
+    
+    def get_channel(self):
+        print("the current channel of ", self.name, " is ", self.channel)
+    

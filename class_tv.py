39eea9d2d48d5a6b704b5ea56f73270c9cda1828
EnzoPinon <mrlorenzopinon@gmail.com>
2023-05-17 23:29:51 +0800

    add class_tv.py

---
+++ class_tv.py
@@ -0,0 +1,7 @@
+class TV:
+    def __init__(self, name, volume, channel, is_on):
+        self.name = name
+        self.volume = volume
+        self.channel = channel
+        self.is_on = is_on
+    

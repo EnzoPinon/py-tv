0a2495b1ef7d71e54a1baab99a8e4489afc60e96
EnzoPinon <mrlorenzopinon@gmail.com>
2023-05-18 11:27:46 +0800

    demo: error for strings on vol/channel

--- test_tv.py
+++ test_tv.py
@@ -6,5 +6,12 @@ tv01 = TV("HirayaTV", 50, 120, False)
 
 tv02 = TV("NieselTV", 30, 900, True)
 
+# demonstrate viewing all stats of the TV
+
 tv01.view()
 tv02.view()
+
+# Demonstration of error message in setting channel/volume as string
+tv01.set_channel("hello")
+
+tv02.set_volume("hi")
